ac7709bb7f40b80e1caef71434cfba254d16788d
volshibenik <volshibenik@gmail.com>
2017-09-22 16:26:07 +0300

    trial and error stage:)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -18,4 +18,36 @@ class GuessingGame {
     }
 }
 
+		class GG {
+      
+      constructor () {
+   //     this.range = [0,0];
+      }
+      
+      setRange (min, max) {
+        this.range = [min,max]
+      }
+      
+      guess() {
+        return (this.range[1]-this.range[0])/2
+      }
+      
+    }
+		
+		
+		
+		const number = 409;
+    const game = new GG();
+    game.setRange(0, 4048)
+		
+		
+		console.log(game.range);
+    
+    console.log(game.guess());
+    
+    console.log(game);
+
+
+
+
 module.exports = GuessingGame;
