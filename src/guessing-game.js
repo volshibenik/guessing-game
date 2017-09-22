87b7db6d004544c770916d5636e3af899abe5987
volshibenik <volshibenik@gmail.com>
2017-09-22 17:35:28 +0300

    100p and my thoughts

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -29,5 +29,7 @@ class GuessingGame {
   }
 
 }
-
+/*
+Description of task is quite confusing, i was thinking all the time that lower and greater methods should be somehow linked with guess and called only if neede d and not like in tests: hardcoded sequence of lower/greater. That's why i had condition and tried to compare my result with 'number', but couldn't reach it))
+*/
 module.exports = GuessingGame;
