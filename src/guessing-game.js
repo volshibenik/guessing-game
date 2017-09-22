237108a70c6c363477f7c72674e352434f2ea9d7
volshibenik <volshibenik@gmail.com>
2017-09-22 17:23:09 +0300

    damn it

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,51 +1,37 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-			return 409
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
-
-		class GG {
+   constructor () {
       
-      constructor () {
-   //     this.range = [0,0];
-      }
-      
-      setRange (min, max) {
+    }
+    setRange (min, max) {
         this.range = [min,max]
       }
       
-      guess() {
-        return (this.range[1]-this.range[0])/2
-      }
+    guess() {
+      return Math.round((this.range[1]+this.range[0])/2);
       
     }
-		
-		
-		
-		const number = 409;
-    const game = new GG();
-    game.setRange(0, 4048)
-		
-		
-		console.log(game.range);
+    lower() {
+      if (this.guess() > number ) {
+        this.setRange(this.range[0], this.guess())
+      }
+
+    }
     
-    console.log(game.guess());
-    
-    console.log(game);
+    greater() {
+      if (this.guess() < number ) {
+        this.setRange(this.guess(),this.range[1])
+      }
+
+    }
+  
+  
+  
+  
+  
+  
+   
+  
+}
 
 
 
