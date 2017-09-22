aad0bcb39681049e5886ccb27660ece67290f7ef
volshibenik <volshibenik@gmail.com>
2017-09-22 17:34:52 +0300

    nailed it

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,39 +1,33 @@
 class GuessingGame {
-   constructor () {
+  
+  setRange (min, max) {
       
-    }
-    setRange (min, max) {
-        this.range = [min,max]
-      }
+    this.range = [min,max]
       
-    guess() {
-      return Math.round((this.range[1]+this.range[0])/2);
+  }
       
-    }
-    lower() {
-      if (this.guess() > number ) {
-        this.setRange(this.range[0], this.guess())
-      }
-
-    }
+  guess() {
     
-    greater() {
-      if (this.guess() < number ) {
+    return Math.round((this.range[1]+this.range[0])/2);
+      
+  }
+  
+  lower() {
+    
+  //    if (this.guess() > number ) {
+        this.setRange(this.range[0], this.guess())
+  //    }
+
+  }
+    
+  greater() {
+    
+ //     if (this.guess() < number ) {
         this.setRange(this.guess(),this.range[1])
-      }
+ //     }
 
-    }
-  
-  
-  
-  
-  
-  
-   
-  
+  }
+
 }
-
-
-
 
 module.exports = GuessingGame;
